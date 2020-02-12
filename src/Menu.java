85d2ce44a178c9910f3c16463f6477e501d7e3ab
Alex <nyt604@gmail.com>
2020-02-12 13:55:15 +0100

    Add comments and add blank space to header method
    added the blank space so we can use it in chains without having to run a forloop every time

--- src/Menu.java
+++ src/Menu.java
@@ -12,43 +12,38 @@ public class Menu {
     {
     }
 
-    public void display()
+    public void display() //to keep the code clean we put display in its own class
     {
-        Scanner input = new Scanner(System.in);
-        boolean run=true;
-        int selection;
+        Scanner input = new Scanner(System.in); //scanner for user input
+        boolean run=true; //boolean to be able to exit runtime without need of class specific console commands
+        int selection; //to register input
         while (run)
         {
-            screenNumber = 1;
-            switch (screenNumber)
+            headerDisplay("FOOTBALL CLUB");
+            System.out.println("what would you like to do?");
+            System.out.println("1) See all current players");
+            System.out.println("2) Add a new player");
+            System.out.println("3) Search for a player");
+            System.out.println("4) Delete a current player");
+            System.out.println("5) Close the program");
+            selection = intCheck(1,5);
+            switch (selection)
             {
-                case 1:
-                {
-                    headerDisplay("FOOTBALL CLUB");
-                    System.out.println("what would you like to do?");
-                    System.out.println("1) See all current players");
-                    System.out.println("2) Add a new player");
-                    System.out.println("3) Search for a player");
-                    System.out.println("4) Delete a current player");
-                    System.out.println("5) Close the program");
-                    selection = intCheck(1,5);
-                    switch (selection)
-                    {
-                        case 1:
-                            break;
-                        case 2:
-                            break;
-                        case 3:
-                            break;
-                        case 4:
-                            break;
-                        case 5:
-                            run = false;
-                    }
+                case 1: //show a list of all current players from linked list
+                    headerDisplay("CURRENT PLAYERS");
+                    break;
+                case 2: //Adds a new player to linked list
+                    headerDisplay("CREATE A NEW PLAYER");
+                    break;
+                case 3: //Searchers for a player in the linked list
+                    headerDisplay("SEARCH FOR A PLAYER");
+                    break;
+                case 4: //Delete a player from the linked list
+                    headerDisplay("DELETE A PLAYER");
+                    break;
+                case 5: //exit the runtime
+                    run = false;
 
-
-
-                }
             }
         }
 
@@ -63,7 +58,7 @@ public class Menu {
 
 
 
-    private int intCheck(int min,int max)
+    private int intCheck(int min,int max) //verify that the user input an int and that the int is in the desired range
     {
         int selection;
         Scanner console = new Scanner(System.in);
@@ -90,9 +85,13 @@ public class Menu {
 
 
     //region Menu decoration
-    private void headerDisplay(@NotNull String header)
+    private void headerDisplay(String header) //header for all menu locations with auto centering spacing
     {
-        decorationSpace = (lineSpacer-header.length())/2;
+        for (int i = 0; i<80; i++)
+        {
+            System.out.println();
+        }
+        decorationSpace = (lineSpacer-header.length())/2; //to measure the space needed to center the text
         for (int i=0;i<lineSpacer;i++)
         {
             System.out.print("-");
@@ -100,7 +99,6 @@ public class Menu {
         System.out.println();
         blankSpace(decorationSpace);
         System.out.print(header);
-        blankSpace(decorationSpace);
         System.out.println();
         for (int i=0; i<lineSpacer; i++)
         {
@@ -109,7 +107,7 @@ public class Menu {
         System.out.println();
     }
 
-    private void blankSpace( int amount )
+    private void blankSpace( int amount ) //Method to add blank spaces
     {
         for (int i =0; i< amount; i++)
         {
