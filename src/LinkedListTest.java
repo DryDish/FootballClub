b04af162d0574a60ff3ce24ec175b30d5e17519d
Cristian <cristi.valentin29@gmail.com>
2020-02-13 17:29:39 +0100

    Comment Appropriately
    Descriptive Description

---
+++ src/LinkedListTest.java
@@ -0,0 +1,13 @@
+public class LinkedListTest {
+
+    private Node head; // First node of the list
+
+    LinkedListTest () {
+
+    }
+
+    public void add ( Player data ) {
+        
+    }
+
+}
---
+++ src/Node.java
@@ -0,0 +1,35 @@
+public class Node {
+
+    private Player data;
+    private Node next;
+
+    public Node () {
+        this(null, null);
+    }
+
+    public Node ( Player data ) {
+        this.data = data;
+        this.next = null;
+    }
+
+    public Node ( Player data, Node next ) {
+        this.data = data;
+        this.next = next;
+    }
+
+    public Player getData() {
+        return data;
+    }
+
+    public void setData(Player data) {
+        this.data = data;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+}
